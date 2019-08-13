ed85fcf1131638bd85b291f4a7645c5ffea1165e
zeraMe1ster <sauhard.eg@gmail.com>
2019-08-13 19:28:49 +0545

    Created data Models for the app

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player : Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
+        
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
@@ -20,5 +27,24 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
  
-
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "womens")
+    }
+    
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "co-ed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 }
